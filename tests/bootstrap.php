d53459eab2c344ea292062a6fa8ca22d6baf5803
Eugene Leonovich <gen.work@gmail.com>
2014-08-13 13:31:38 +0200

    Use "autoload-dev" for autoloading tests

--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$loader = require __DIR__.'/../vendor/autoload.php';
-$loader->addPsr4('CallableArgumentsResolver\Tests\\', __DIR__);
-$loader->addPsr4('CallableArgumentsResolver\Tests\\', __DIR__.'/callables');
-
-require __DIR__.'/callables/functions.php';
-require __DIR__.'/utils.php';
